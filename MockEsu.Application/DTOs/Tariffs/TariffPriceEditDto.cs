e05a6f74e1065dfb6babc45b8080c19bc033e4d6
meckbaig <meckbaig@yandex.ru>
2024-01-31 15:47:27 +0600

    Add tariff price edit DTO

---
+++ MockEsu.Application/DTOs/Tariffs/TariffPriceEditDto.cs
@@ -0,0 +1,30 @@
+﻿using MockEsu.Application.Common;
+using MockEsu.Domain.Entities.Traiffs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using MockEsu.Domain.Common;
+
+namespace MockEsu.Application.DTOs.Tariffs;
+
+public record TariffPriceEditDto : BaseDto, IEntityWithId
+{
+    public int Id { get; init; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+             CreateMap<TariffPrice, TariffPriceEditDto>()
+                .ForMember(m => m.Id, opt => opt.MapFrom(o => o.Id))
+                .ReverseMap();
+        }
+    }
+}
