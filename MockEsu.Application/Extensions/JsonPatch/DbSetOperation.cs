abc95fe3c23ee4ec8a1dec12f0d9dfe2b0c6e9e1
Семён Бородяев <meckbaig@yandex.ru>
2024-02-14 18:05:58 +0600

    Add dbSet operation for json patch validation

---
+++ MockEsu.Application/Extensions/JsonPatch/DbSetOperation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.EntityFrameworkCore;
+using MockEsu.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockEsu.Application.Extensions.JsonPatch;
+
+public class DbSetOperation<TModel> : Operation<DbSet<TModel>>, IDbSetOperation where TModel : BaseEntity
+{
+    public string dtoPath { get; set; }
+}
+
+public class ReguldarOperation : Operation
+{
+    public override string ToString() => path;
+}
+
+public interface IDbSetOperation
+{
+    string dtoPath { get; set; }
+}
