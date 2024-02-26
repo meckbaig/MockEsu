b15afc30c0a5e715cacd3603d66306e2ef542b77
Семён Бородяев <meckbaig@yandex.ru>
2024-02-26 12:22:36 +0600

    Change test folders locations

---
+++ Tests/MockEsu.Application.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace MockEsu.Application.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- Tests/MockEsu.Application.Tests/Services/Kontragents/GetKontragentsQueryTests.cs
+++ Tests/MockEsu.Application.Tests/Services/Kontragents/GetKontragentsQueryTests.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using MockEsu.Application.Common.Interfaces;
 using MockEsu.Application.DTOs.Kontragents;
-using MockEsu.Application.Extensions.DataBaseProvider;
 using MockEsu.Application.Services.Kontragents;
 using MockEsu.Domain.Entities;
 using MockEsu.Infrastructure.Data;
@@ -36,26 +35,11 @@ public class GetKontragentsQueryTests //: KontragentsApiFactory
         _cache = Substitute.For<IDistributedCache>();
     }
 
-    //[Fact]
-    //public async Task GetKontragentsQuery_ReturnList_WhenNothingProvided()
-    //{
-    //    // Arrange
-    //    var query = new GetKontragentsQuery();
-
-    //    // Act
-    //    var result = await _handler.Handle(query, default);
-
-    //    // Assert
-    //    Assert.NotNull(result?.Items);
-    //    Assert.Equal(result?.Items.Count, 13021);
-    //}
-
-
     [Fact]
     public async Task GetKontragentsQuery_ReturnSingle_WhenTake1_DtoCheck()
     {
         // Arrange
-        List<Kontragent> kontragents = new List<Kontragent>()
+        var kontragents = new List<Kontragent>()
         {
             new()
             {
@@ -93,11 +77,13 @@ public class GetKontragentsQueryTests //: KontragentsApiFactory
             RegionString = "Бостандыкский район"
         };
 
-        //var contextMock = new Mock<IAppDbContext>();
-        //contextMock.Setup(x => x.Kontragents).Returns(kontragents);
+        Mock<DbSet<Kontragent>> dbSetMock = CreateDbSetMock(kontragents.AsQueryable());
 
-        var handler = new GetKontragentsQueryHandler(_inMemoryContext, _mapper, _cache);
-        var query = new GetKontragentsQuery { take = 1 };
+        Mock<IAppDbContext> contextMock = new();
+        contextMock.Setup(x => x.Kontragents).Returns(dbSetMock.Object);
+
+        var handler = new GetKontragentsQueryHandler(contextMock.Object, _mapper, _cache);
+        var query = new GetKontragentsQuery();
 
         // Act
         var result = await handler.Handle(query, default);
@@ -112,5 +98,15 @@ public class GetKontragentsQueryTests //: KontragentsApiFactory
             object v2 = prop.GetValue(result.Items[0]);
             Assert.Equal(v1, v2);
         }
+    }
+
+    private static Mock<DbSet<T>> CreateDbSetMock<T>(IQueryable<T> queryable) where T : class
+    {
+        var dbSetMock = new Mock<DbSet<T>>();
+        dbSetMock.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+        dbSetMock.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+        dbSetMock.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+        dbSetMock.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+        return dbSetMock;
     }
 }
