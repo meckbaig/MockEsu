62c7fb31de0702f9098636f70f334714d95397ce
Семён Бородяев <meckbaig@yandex.ru>
2024-03-19 16:09:12 +0500

    Add more json patch roots testing

--- MockEsu.Web/Controllers/V1/KontragentsController.cs
+++ MockEsu.Web/Controllers/V1/KontragentsController.cs
@@ -1,4 +1,6 @@
-﻿using MockEsu.Application.Services.Kontragents;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using MockEsu.Application.DTOs.Kontragents;
+using MockEsu.Application.Services.Kontragents;
 using MockEsu.Domain.Enums;
 using MockEsu.Infratructure.Authentification;
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
@@ -30,6 +32,15 @@ public class KontragentsController : ControllerBase
     public async Task<ActionResult<GetKontragentsResponse>> GetList([FromQuery] GetKontragentsQuery query)
     {
         var result = await _mediator.Send(query);
+        return result.ToJsonResponse();
+    }
+
+    [HttpPatch]
+    public async Task<ActionResult<JsonPatchKontragentsResponse>> UpdateList(
+        [FromBody] JsonPatchDocument<KontragentEditDto> items)
+    {
+        JsonPatchKontragentsCommand command = new() { Patch = items };
+        var result = await _mediator.Send(command);
         return result.ToJsonResponse();
     }
 }
